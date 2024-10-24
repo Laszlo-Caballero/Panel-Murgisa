b5da3be34a2012d1cb95fb5fd766e3b4bfc1086b
luciana-31 <lgogny74@gmail.com>
2024-10-24 02:40:05 -0500

    Solucion Conflictos

--- ServiciosConstruccion/obj/Debug/net8.0-windows/CapaPresentacion.AssemblyInfo.cs
+++ ServiciosConstruccion/obj/Debug/net8.0-windows/CapaPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec0e5ef512d7081d91670492abdf3528a609d9fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f1da0e7daf4c0360ddfd2dbaf91acfbb751b65")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

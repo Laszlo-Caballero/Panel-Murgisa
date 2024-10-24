0c8774da0ecc8c5901fed59318a76c8d7c6a2aee
laszlocaballero <laszlocaballero@gmail.com>
2024-10-24 00:59:52 -0500

    se ordeno en carpetas

--- CapaPresentacion/obj/Debug/net8.0-windows/CapaEntidad.AssemblyInfo.cs
+++ CapaPresentacion/obj/Debug/net8.0-windows/CapaEntidad.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaEntidad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ea22c6cd0f68ec4eb2e0e045a378405a577d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaEntidad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaEntidad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ServiciosConstruccion/obj/Debug/net8.0-windows/CapaPresentacion.AssemblyInfo.cs
+++ ServiciosConstruccion/obj/Debug/net8.0-windows/CapaPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ea22c6cd0f68ec4eb2e0e045a378405a577d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

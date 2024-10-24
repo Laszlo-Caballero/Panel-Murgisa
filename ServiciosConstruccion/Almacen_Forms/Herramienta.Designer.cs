c2fa39e01c1717d03f5871bcfd4377cfbb386d52
laszlocaballero <laszlocaballero@gmail.com>
2024-10-24 03:08:30 -0500

    form herramientas

---
+++ ServiciosConstruccion/Almacen_Forms/Herramienta.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CapaPresentacion.Almacen_Forms
+{
+    partial class Herramienta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Herramienta
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(927, 496);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Herramienta";
+            Text = "Herramienta";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}

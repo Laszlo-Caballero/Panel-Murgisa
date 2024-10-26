1b0704e1c3cdf7238d6720aa386c5d0f5902ad24
AntonellaHonores <antonellahonores15@gmail.com>
2024-10-25 22:44:35 -0500

    actualicé eliminación de almacén

--- ServiciosConstruccion/PanelVendedor.cs
+++ ServiciosConstruccion/PanelVendedor.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using CapaPresentacion;
-using CapaPresentacion.Almacen_Forms;
+
 
 namespace Capa_Presentacion
 {
--- ServiciosConstruccion/Program.cs
+++ ServiciosConstruccion/Program.cs
@@ -1,6 +1,6 @@
 using Capa_Presentacion;
 using CapaPresentacion;
-using CapaPresentacion.Almacen_Forms;
+
 
 namespace ServiciosConstruccion
 {

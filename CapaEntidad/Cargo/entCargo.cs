0de5ffa5405f4dbfed73bba38b1010a3c68aeb2a
luciana-31 <lgogny74@gmail.com>
2024-11-27 03:59:24 -0500

    Agregar entCargo

---
+++ CapaEntidad/Cargo/entCargo.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad.Cargo
+{
+    public class entCargo
+    {
+        public int id {  get; set; }
+
+        public string cargo { get; set; }
+
+        public string desc {  get; set; }
+
+        public float sueldo { get; set; }
+
+        public bool estado { get; set; }
+    }
+}
+        

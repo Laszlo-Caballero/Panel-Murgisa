0599729f82128a79bbbf18943536eb2d5a9a59ec
ManuL0129 <manumonla@hotmail.com>
2024-11-27 03:54:24 -0500

    Agregue MantenedorCondicion

---
+++ CapaAccesoDatos/Condicion/datCondicion.cs
@@ -0,0 +1,115 @@
+using CapaEntidad.Condicion;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAccesoDatos.Condicion
+{
+    public class datCondicion
+    {
+        private static readonly datCondicion _instancia = new datCondicion();
+
+        public static datCondicion Instancia
+        {
+            get { return _instancia; }
+        }
+
+        public List<entCondicion> listarCondicion()
+        {
+            SqlCommand cmd = null;
+            List<entCondicion> lista = new List<entCondicion>();
+            try
+            {
+                SqlConnection cn = Conexion.Instacia.Conectar();
+                cn.Open();
+                cmd = new SqlCommand("listarCondicion", cn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    entCondicion nuevo = new entCondicion();
+                    lista.Add(nuevo);
+                }
+            }
+            catch (Exception ex) {
+                throw ex;
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+            return lista;
+        }
+
+        public bool agregarCondicion(entCondicion nuevo)
+        {
+            SqlCommand cmd = null;
+            bool agregar = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instacia.Conectar();
+                cn.Open();
+                cmd = new SqlCommand("agregarCondicion", cn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                int rows = cmd.ExecuteNonQuery();
+                agregar = rows >= 1;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+            return agregar;
+        }
+
+        public bool actualizarCondicion(entCondicion nuevo)
+        {
+            SqlCommand cmd = null;
+            bool actualizar = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instacia.Conectar();
+                cn.Open();
+                cmd = new SqlCommand("actualizarCondicion", cn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                int rows = cmd.ExecuteNonQuery();
+                actualizar = rows >= 1;
+            }
+            catch (Exception ex) { 
+                throw ex;
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+            return actualizar;
+        }
+
+        public bool deshabilitarCondicion(int id)
+        {
+            SqlCommand cmd = null;
+            bool deshabilitar = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instacia.Conectar();
+                cn.Open();
+                cmd = new SqlCommand("deshabilitarCondicion", cn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@idCondicion", id);
+                int rows = cmd.ExecuteNonQuery();
+                deshabilitar = rows >= 1;
+            }
+            catch (Exception ex) { throw ex; }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+            return deshabilitar;
+        }
+    }
+}
---
+++ CapaLogica/Condicion/logCondicion.cs
@@ -0,0 +1,39 @@
+using CapaAccesoDatos.Condicion;
+using CapaEntidad.Condicion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica.Condicion
+{
+    public class logCondicion
+    {
+        private static readonly logCondicion _instancia = new logCondicion();
+
+        public static logCondicion Instancia
+        {
+            get { return _instancia; }
+        }
+        public List<entCondicion> listarCondicion()
+        {
+            return datCondicion.Instancia.listarCondicion();
+        }
+
+        public bool agregarCondicion(entCondicion nuevo)
+        {
+            return datCondicion.Instancia.agregarCondicion(nuevo);
+        }
+
+        public bool actualizarCondicion(entCondicion nuevo)
+        {
+            return datCondicion.Instancia.actualizarCondicion(nuevo);
+        }
+
+        public bool deshablitarServicio(int id)
+        {
+            return datCondicion.Instancia.deshabilitarCondicion(id);
+        }
+    }
+}

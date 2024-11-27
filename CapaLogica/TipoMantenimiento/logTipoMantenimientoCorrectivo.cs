8d9882279f8145c363faf5ceb90b6af3fcdd166f
Fernandavasq26 <vasquezfernanda334@gmail.com>
2024-11-27 04:49:49 -0500

    Agregue entTipoMantemientoCorrectivo logTipoMantemientoCorrectivo

---
+++ CapaLogica/TipoMantenimiento/logTipoMantenimientoCorrectivo.cs
@@ -0,0 +1,39 @@
+using CapaAccesoDatos.TipoMantenimiento;
+using CapaEntidad.TipoMantenimiento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica.TipoMantenimiento
+{
+    public class logTipoMantenimientoCorrectivo
+    {
+        private static readonly logTipoMantenimientoCorrectivo _instancia = new logTipoMantenimientoCorrectivo();
+
+        public static logTipoMantenimientoCorrectivo Instancia
+        {
+            get { return _instancia; }
+        }
+        public List<entTipoMantenimientoCorrectivo> listarTipoMantenimientoCorrectivo()
+        {
+            return datTipoMantenimientoCorrectivo.Instancia.listarTipoMantenimientoCorrectivo();
+        }
+
+        public bool agregarTipoMantenimientoCorrectivo(entTipoMantenimientoCorrectivo nuevo)
+        {
+            return datTipoMantenimientoCorrectivo.Instancia.agregarTipoMantenimientoCorrectivo(nuevo);
+        }
+
+        public bool actualizarTipoMantenimientoCorrectivo(entTipoMantenimientoCorrectivo nuevo)
+        {
+            return datTipoMantenimientoCorrectivo.Instancia.actualizarTipoMantenimientoCorrectivo(nuevo);
+        }
+
+        public bool deshablitarServicio(int id)
+        {
+            return datTipoMantenimientoCorrectivo.Instancia.deshabilitarTipoMantenimientoCorrectivo(id);
+        }
+    }
+}
